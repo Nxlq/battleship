7caeca86e9c7ca5d950b9ac580e25d05f101c129
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-04-27 15:26:41 -0400

    create test to check if ships can be properyly placed vertically on the gameboard via setShip method

--- gameboard.test.js
+++ gameboard.test.js
@@ -16,3 +16,20 @@ test("gameboard should place ships at specific coordinates HORIZONTALLY by calli
 
   expect(gameboard.getBoard()).toEqual(mockBoard);
 });
+
+test("gameboard should be able to place ships at specific coordinates VERTICALLY by calling the setShip method and passing in a third argument", () => {
+  const gameboard = GameboardFactory();
+  gameboard.setShip([0, 0], "carrier", "v");
+
+  const mockBoard = new Map([]);
+  for (let i = 0; i < 10; i += 1) {
+    for (let j = 0; j < 10; j += 1) {
+      mockBoard.set([i, j].toString(), "water");
+    }
+  }
+  for (let i = 0; i < 5; i += 1) {
+    mockBoard.set(`${i},0`, "carrier");
+  }
+
+  expect(gameboard.getBoard()).toEqual(mockBoard);
+});
