f1bd4754c60cf82a865b01d6ce17370df65f6eef
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-04-30 19:39:25 -0400

    create function to assign coords to the game grids dom via coords attribute

---
+++ src/game-view.js
@@ -0,0 +1,20 @@
+function assignGameBoardCoords() {
+  const gameBoards = document.querySelectorAll(".game-board");
+
+  gameBoards.forEach((gameBoard) => {
+    let y = 0;
+    let x = 0;
+    const grid = [...gameBoard.children];
+    grid.forEach((gridSquare) => {
+      if (x > 9) {
+        x = 0;
+        y += 1;
+      }
+
+      gridSquare.setAttribute("coords", `${y}, ${x}`);
+      x += 1;
+    });
+  });
+}
+
+export default assignGameBoardCoords;
